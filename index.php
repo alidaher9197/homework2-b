a5ab5cee1c37189bf7fc2cd888b27401f040b9fb
ali <alidaher9197@gmail.com>
2025-08-30 19:54:15 +0300

    html tags till highlited text

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 2</title>
+    <link rel="icon" type="html_icon.png" href="assets/html_icon.png" class="title-logo"><!--this image is in the title-->
+</head>
+<body>
+    <h1>Assiggnment 2</h1>
+    <p>
+        This assignment covers various <strong>HTML text elements</strong>
+    </p>
+    <hr>
+    <h2>Headings</h2>
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
+    <hr>
+    <h2>Text Formatting</h2>
+    <p>This is normal paragraph.</p>
+    <p>This is <em>emphasized</em> text.</p>
+    <p>This is <u>underlined</u> text</p>
+    <p>This is <mark>highlighted</mark> text</p>
+</body>
+</html>
